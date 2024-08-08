a1bf5744665ed77c8d632caad6d3ab0fd06e8ddd
jenniyus <jenniyus.02@gmail.com>
2024-08-08 18:55:02 +0700

    font

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
+import { useFonts } from "expo-font";
 import StackNavigator from "./StackNavigator";
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    'Clash Grotesk': require('./assets/fonts/ClashGrotesk.ttf'),
+    'Satoshi': require('./assets/fonts/Satoshi.ttf'),
+    // 'SF Pro Text': require('./assets/fonts/SfProText.OTF')
+  });
   return (
     <>
       <StackNavigator />
