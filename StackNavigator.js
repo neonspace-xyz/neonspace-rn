86809fcbdf85a4bd59742055d76b41b1adbe5b72
Andreas Billy Sutandi <andreas.sutandi@atibusinessgroup.com>
2024-08-21 17:30:43 +0700

    add drawer navigation

--- StackNavigator.js
+++ StackNavigator.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import React, { useState } from 'react';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './screens/Login';
 import Mint from './screens/Mint';
@@ -7,10 +7,16 @@ import Main from './screens/Main';
 import ReferralCode from './screens/ReferralCode';
 import { AuthProvider } from './components/AuthProvider';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Button, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Color } from './GlobalStyles';
 
 const StackNavigator = () => {
   const Stack = createNativeStackNavigator();
-  // const Drawer = createDrawerNavigator();
+  const Drawer = createDrawerNavigator();
+  const [userInfo, setUserInfo] = useState();
+
   const linking = {
     prefixes: ['exp://192.168.1.4:8081'],
     config: {
@@ -19,13 +25,95 @@ const StackNavigator = () => {
       },
     },
   };
+  const StackNavigatorComponent = () => {
+    
+    return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="ReferralCode"
+        component={ReferralCode}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Mint"
+        component={Mint}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Main"
+        component={Main}
+        options={{ headerShown: false }} />
 
-  
+    </Stack.Navigator>
+  )};
+
+  const CustomDrawerContent = (props) => {
+    
+    const navigation = useNavigation();
+
+    return (
+    <SafeAreaView style={styles.drawerContent}>
+      <View style={styles.drawerSection}>
+
+        {userInfo?.profile_image ? (
+          <Image
+            style={[styles.myProfileItem]}
+            contentFit="cover"
+            source={userInfo.profile_image}
+          />
+        ) : (
+          <Image
+            style={[styles.myProfileItem]}
+            contentFit="cover"
+            source={require("./assets/photo.png")}
+          />
+        )}
+
+        <Text style={{color:"white"}}>Name</Text>
+        <Text style={{color:"white"}}>@endlessmeeee</Text>
+        <Text style={{color:"white"}}>Wallet Address: 0xe...dhv</Text>
+
+        <View style={{marginTop:50}}>
+          <TouchableOpacity>
+            <Text style={styles.drawerMenu}>Post Crowdsource</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.drawerMenu}>My Full Bio</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {
+            navigation.navigate('Profile', {
+              screen: 'MyProfile4',
+              params: {
+                reset: true,
+              },
+            })
+          }
+          }>
+            <Text style={styles.drawerMenu}>Wallet</Text>
+          </TouchableOpacity>
+        </View>
+        {/* <Text style={styles.title}>Custom Drawer</Text>
+        <Button title="Home" onPress={() => props.navigation.navigate('Home')} />
+        <Button title="Profile" onPress={() => props.navigation.navigate('Profile')} /> */}
+      </View>
+    </SafeAreaView>
+  )};
+
   return (
-    <NavigationContainer linking={linking}>
-      <AuthProvider>
-        {/* <Drawer.Navigator initialRouteName="Home"> */}
-          <Stack.Navigator>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer linking={linking}>
+        <AuthProvider>
+          {/* {StackNavigatorComponent()} */}
+          {/* <StackNavigatorComponent/> */}
+          <Drawer.Navigator drawerContent={(props) => <CustomDrawerContent {...props} />}>
+            <Drawer.Screen name="DrawerMain" component={StackNavigatorComponent} options={{ headerShown: false }} />
+          </Drawer.Navigator>
+          {/* <Stack.Navigator>
             <Stack.Screen
               name="ReferralCode"
               component={ReferralCode}
@@ -46,11 +134,41 @@ const StackNavigator = () => {
               component={Main}
               options={{ headerShown: false }} />
 
-          </Stack.Navigator>
-        {/* </Drawer.Navigator> */}
-      </AuthProvider>
-    </NavigationContainer>
+          </Stack.Navigator> */}
+        </AuthProvider>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  drawerMenu:{
+    color:"white", fontWeight: "900", marginTop: 20
+  },
+  drawerContent: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: Color.colorDarkslategray_100,
+  },
+  drawerSection: {
+    marginVertical: 16,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  myProfileItem: {
+    width: 90,
+    height: 90,
+    // margin: 10,
+    borderRadius: 50
+  },
+});
+
 
 export default StackNavigator
--- screens/Main.js
+++ screens/Main.js
@@ -8,10 +8,126 @@ import ProfileStackNavigator from "./ProfileStackNavigator";
 import { TouchableOpacity } from "react-native";
 import NotificationStackNavigator from "./NotificationStackNavigator";
 import CrowdsourceStackNavigator from "./CrowdsourceStackNavigator";
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Main = () => {
   // Create the bottom tab navigator
   const Tab = createBottomTabNavigator();
+  const Drawer = createDrawerNavigator();
+  
+  const CustomDrawerContent = (props) => (
+    <SafeAreaView style={styles.drawerContent}>
+      <View style={styles.drawerSection}>
+        <Text style={{color:"white"}}>Name</Text>
+        <Text style={{color:"white"}}>@endlessmeeee</Text>
+        <Text style={{color:"white"}}>Wallet Address: 0xe...dhv</Text>
+
+        <View style={{marginTop:20}}>
+          <Text style={{color:"white"}}>Post Crowdsource</Text>
+          <Text style={{color:"white"}}>My Full Bio</Text>
+          <Text style={{color:"white"}}>Wallet</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+
+  const TabNavigatorComponent = () => (
+    <Tab.Navigator
+
+      screenOptions={({ route }) => ({
+        tabBarButton: (props) => (
+          <TouchableOpacity
+            {...props}
+            onPress={() => {
+              props.onPress();
+            }}
+          />
+        ),
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Notification') {
+            iconName = focused ? 'notifications' : 'notifications-outline';
+          } else if (route.name === 'Wallet') {
+            iconName = focused ? 'wallet' : 'wallet-outline';
+          } else if (route.name === 'Profile') {
+            iconName = focused ? 'person' : 'person-outline';
+          } else if (route.name === 'Crowdsource') {
+            iconName = focused ? 'logo-usd' : 'logo-usd';
+          } 
+
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+        headerShown: false,
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+        tabBarStyle: {
+          backgroundColor: Color.colorDarkslategray_400, // Change this to your desired background color
+        }
+      })}
+    >
+      <Tab.Screen name="Home" component={PostStackNavigator}
+        listeners={({ navigation }) => ({
+          tabPress: (e) => {
+            navigation.navigate('Home', {
+              screen: 'PostList1',
+              params: {
+                reset: true,
+              },
+            });
+          }
+        })} />
+        <Tab.Screen name="Crowdsource" component={CrowdsourceStackNavigator}
+        listeners={({ navigation }) => ({
+          tabPress: (e) => {
+            navigation.navigate('Crowdsource', {
+              screen: 'CrowdsourceView5',
+              params: {
+                reset: true,
+              },
+            });
+          }
+        })} />
+      <Tab.Screen name="Notification" component={NotificationStackNavigator}
+        listeners={({ navigation }) => ({
+          tabPress: (e) => {
+            navigation.navigate('Notification', {
+              screen: 'Notification2',
+              params: {
+                reset: true,
+              },
+            });
+          }
+        })} />
+      <Tab.Screen name="Wallet" component={WalletStackNavigator}
+        listeners={({ navigation }) => ({
+          tabPress: (e) => {
+            navigation.navigate('Wallet', {
+              screen: 'Wallet3',
+              params: {
+                reset: true,
+              },
+            });
+          }
+        })} />
+      <Tab.Screen name="Profile" component={ProfileStackNavigator}
+        listeners={({ navigation }) => ({
+          tabPress: (e) => {
+            navigation.navigate('Profile', {
+              screen: 'MyProfile4',
+              params: {
+                reset: true,
+              },
+            });
+          }
+        })} />
+    </Tab.Navigator>
+  );
 
   return (
     <Tab.Navigator
@@ -106,7 +222,28 @@ const Main = () => {
           }
         })} />
     </Tab.Navigator>
-  );
+  )
 };
+
+
+const styles = StyleSheet.create({
+  drawerContent: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: Color.colorDarkslategray_100,
+  },
+  drawerSection: {
+    marginVertical: 16,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default Main;
